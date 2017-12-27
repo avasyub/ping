cdcf9b1713c80dd44b048ebbd681b5c55d52f97b
avasyub <avasyubhatia@gmail.com>
2017-12-27 17:17:38 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,54 +1,51 @@
-# chat_client.py
-
+#!/usr/bin/env python3
+"""Script for Tkinter GUI chat client."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread 
 import sys
-import socket
-import select
- 
-def chat_client():
-    if(len(sys.argv) < 3) :
-        print('Usage : python chat_client.py hostname port')
-        sys.exit()
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-     
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-     
-    # connect to remote host
-    try :
-        s.connect((host, port))
-    except :
-        print('Unable to connect')
-        sys.exit()
-     
-    print('Connected to remote host. You can start sending messages')
-    sys.stdout.write('[Me] '); sys.stdout.flush()
-     
-    while 1:
-        socket_list = [sys.stdin, s]
-         
-        # Get the list sockets which are readable
-        ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
-         
-        for sock in ready_to_read:             
-            if sock == s:
-                # incoming message from remote server, s
-                data = sock.recv(4096)
-                if not data :
-                    print('\nDisconnected from chat server')
-                    sys.exit()
-                else :
-                    #print data
-                    sys.stdout.write(data.decode('utf-8'))
-                    sys.stdout.write('[Me] '); sys.stdout.flush()     
-            
-            else :
-                # user entered a message
-                msg = sys.stdin.readline()
-                s.send(msg.encode('utf-8'))
-                sys.stdout.write('[Me] '); sys.stdout.flush() 
+def receive():
+    """Handles receiving of messages."""
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            print("\n"+msg)
+            send_thread = Thread(target=send)
+            send_thread.start()
+            # msg_list.insert(tkinter.END, msg)
+        except OSError:  # Possibly client has left the chat.
+            break
 
-if __name__ == "__main__":
 
-    sys.exit(chat_client())
+def send():  # event is passed by binders.
+    """Handles sending of messages."""
+
+    msg = input("Enter Message : ")
+    print("\n[Me]"+msg)
+    # my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()   
+        # top.quit()    
+    else:
+        receive_thread = Thread(target=receive)
+        receive_thread.start()
+
+
+HOST = input('Enter host: ')
+PORT = input('Enter port: ')
+if not HOST:
+    HOST = "13.126.183.145"
+if not PORT:
+    PORT = 27001
+else:
+    PORT = int(PORT)
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
